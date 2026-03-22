eb12ba30f3ccd01094da69f859f8433352650817
CYouuu <zqwe1230@gmail.com>
2026-03-23 04:47:30 +0800

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by ChengYou on 2026/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
